0e8eb58af36a758bd399ef0612a3025eeccd976f
CARLOS MARIO RESTREPO <cmrestrepoc@unal.edu.co>
2023-05-03 14:41:35 -0500

    Implementing CrudRepository for Productos

---
+++ src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,7 @@
+package com.platzi.platzimarket.persistence.crud;
+
+import com.platzi.platzimarket.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+}
