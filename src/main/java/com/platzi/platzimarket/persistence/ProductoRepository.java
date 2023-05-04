3199667a5cb15d136f846e36d9352bd54553077a
CARLOS MARIO RESTREPO <cmrestrepoc@unal.edu.co>
2023-05-03 19:06:02 -0500

    Query methods and crudRepository methods

--- src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
@@ -4,11 +4,24 @@ import com.platzi.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzi.platzimarket.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll() {
-        return (List<Producto>) productoCrudRepository.findAll();
+        return (List<Producto>) productoCrudRepository.findAll(); // CrudRepository method
+    }
+
+    public List<Producto> getByCategoriaOrdered(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria); // using query method
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoria(idCategoria); // using query method
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad, boolean estado) {
+        return productoCrudRepository.findByCandidadStockLessThanAndEstado(cantidad, estado);
     }
 }
--- src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,18 @@
 package com.platzi.platzimarket.persistence.crud;
 
 import com.platzi.platzimarket.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    @Query(value="SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> getByCategoria(int idCategoria); // Using query
+
+    List<Producto> findByIdCategoria(int idCategoria); // Query Method
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria); // Query Method
+
+    Optional<List<Producto>> findByCandidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
